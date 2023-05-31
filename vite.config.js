436818b90c3bab11adbcbfdfb6f84641d93d8222
Haroon533 <73595693+Haroon533@users.noreply.github.com>
2023-05-31 16:38:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/FGym/'
-    : '/'
-}
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:'/FGym/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
